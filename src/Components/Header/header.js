dca7c3909b6f71d5b0a21a960dd31cc1129896e0
Rinnohara9803 <sagarprajapati9803@gmail.com>
2023-11-14 18:34:21 +0545

    final edit

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import "../../../src/App.css";
 import { useSelector } from "react-redux/es/hooks/useSelector";
@@ -9,27 +9,12 @@ import { useDispatch } from "react-redux";
 import { getLoggedInState } from "../../action-creators/auth_action";
 
 function Header() {
-  const [showMenu, setShowMenu] = useState(false);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [showOptions, setShowOptions] = useState(false);
 
   const toggleOptions = () => {
     setShowOptions(!showOptions);
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const toggleMenu = () => {
-    setShowMenu((prev) => !prev); // Corrected the toggle logic
   };
 
   const authState = useSelector((state) => {
@@ -38,66 +23,57 @@ function Header() {
 
   const isLoggedIn = authState.loggedIn;
   const user = authState.user;
+  console.log(authState.token);
 
   return (
     <header
       className="my-0 mr-auto ml-auto fixed inset-x-0 top-0 py-2 pb-4 lin-grad bg-gray-100"
       style={{ zIndex: 1 }}
     >
-      <nav className="w-4/6 mx-auto flex justify-around my-0 mx-auto items-center">
-        {windowWidth < 800 && (
-          <div className=" mt-1.5 pr-4">
-            <FontAwesomeIcon
-              icon={faBars}
-              className="text-2xl cursor-pointer transition-transform duration-500 ease-in-out"
-              style={{
-                transform: showMenu ? "rotate(90deg)" : "rotate(0deg)",
-              }}
-              onClick={toggleMenu}
-            />
-          </div>
-        )}
-        <div className="flex-grow text-center sm:flex lg:justify-start justify-center">
-          <div className="flex items-center justify-center">
-            <FontAwesomeIcon icon={faPlus} className="lg:text-2xl sm:text-md" />
-          </div>
-          <p className="lg:text-2xl inline-block mx-2 sm:sm">MEDICAL</p>
+      <nav className="w-4/6 flex justify-around my-0 mx-auto items-center">
+        <div className="flex justify-end">
+          <FontAwesomeIcon icon={faPlus} className="pr-2 mt-1.5 text-2xl" />
+          <p className="text-2xl">MEDICAL</p>
         </div>
-        {windowWidth >= 800 && (
-          <div className="mt-1.5 font-semibold">
-
+        <div className="mt-1.5 font-semibold">
+          <Link
+            to="/"
+            className="mr-7 hover:text-orange-400 transition-all duration-300"
+          >
+            Home
+          </Link>
+          <Link
+            to="/find-doctors"
+            className="mr-7 hover:text-orange-400 transition-all duration-300"
+          >
+            Find Doctors
+          </Link>
+          {user !== null && authState.role !== "admin" && (
             <Link
-              to="/"
+              to="/my_appointments"
               className="mr-7 hover:text-orange-400 transition-all duration-300"
             >
-              Home
+              My Appointments
             </Link>
-              to="/find-doctors"
+          )}
+          {user !== null && authState.role !== "admin" && (
+            <Link
+              to="/medical_history_list"
               className="mr-7 hover:text-orange-400 transition-all duration-300"
             >
-              Find Doctors
+              Medical History
             </Link>
-            {user.role !== "admin" && (
-              <Link
-                to="/my_appointments"
-                className="mr-7 hover:text-orange-400 transition-all duration-300"
-              >
-                My Appointments
-              </Link>
-            )}
-            {user !== null && user.role === "admin" && (
-              <Link
-                to="/doctor-applications"
-                className="mr-7 hover:text-orange-400 transition-all duration-300"
-              >
-                Doctor-Applications
-              </Link>
-            )}
-          </div>
-        )}
-
-        {user !== null && !isLoggedIn && (
-
+          )}
+          {user !== null && authState.role === "admin" && (
+            <Link
+              to="/doctor-applications"
+              className="mr-7 hover:text-orange-400 transition-all duration-300"
+            >
+              Doctor-Applications
+            </Link>
+          )}
+        </div>
+        {user === null && !isLoggedIn && (
           <Link
             to="/login"
             className="bg-transparent text-orange-400 py-1.5 px-4 border border-solid border-orange-400 rounded-sm hover:bg-orange-400 hover:text-white transition-all duration-300  ease-in-out"
@@ -106,11 +82,11 @@ function Header() {
           </Link>
         )}
         {user !== null && isLoggedIn && (
-          <div className="relative flex lg:w-52 justify-center">
+          <div className="relative w-52 pl-8">
             <img
               src={`http://localhost:3009/assets/${user.image}`}
               alt={user.name}
-              className="flex  h-10 w-10 rounded-full  border border-gray-400 cursor-pointer justify-end"
+              className="flex h-10 w-10 rounded-full  border border-gray-400 cursor-pointer justify-end"
               onClick={toggleOptions}
             />
             {showOptions && (
@@ -137,44 +113,6 @@ function Header() {
           </div>
         )}
       </nav>
-      {windowWidth < 800 && (
-        <div
-          className={`bg-white w-4/6 mx-auto rounded shadow text-center lin-grad text-slate-200 transition-all duration-500 ease-in-out ${
-            showMenu ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
-          } overflow-hidden`}
-        >
-          <div className={`${showMenu ? "block" : "hidden"}`}>
-            <Link
-              to="/"
-              className="block mb-2 hover:text-orange-400 text-gray-700 transition-all duration-300"
-            >
-              Home
-            </Link>
-            <Link
-              to="/find-doctors"
-              className="block mb-2 hover:text-orange-400  text-gray-700 transition-all duration-300"
-            >
-              Find Doctors
-            </Link>
-            {user.role !== "admin" && (
-              <Link
-                to="/my_appointments"
-                className="block mb-2 hover:text-orange-400  text-gray-700 transition-all duration-300"
-              >
-                My Appointments
-              </Link>
-            )}
-            {user !== null && user.role === "admin" && (
-              <Link
-                to="/doctor-applications"
-                className="block mb-2 hover:text-orange-400 transition-all duration-300"
-              >
-                Doctor-Applications
-              </Link>
-            )}
-          </div>
-        </div>
-      )}
     </header>
   );
 }
